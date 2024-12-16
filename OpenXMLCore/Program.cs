6478d35f479fed128f142a3777aba98e7462c0df
吴冰 <wubing@xuanit.com>
2024-12-16 14:05:15 +0800

    添加项目文件。

---
+++ OpenXMLCore/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenXMLCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input the file name:");
+
+            string fileName = Console.ReadLine();
+
+            if(!fileName.EndsWith(".docx"))
+            {
+                fileName += ".docx";
+            }
+
+            OpenXMLExecute.CreateDocx(@"C:\Users\usr\Code\MyProject\OpenXMLDemo" + fileName);
+        }
+    }
+}
